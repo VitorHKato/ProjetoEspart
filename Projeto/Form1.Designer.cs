fbe789abf4c0a7710559baab8a82839950c6a857
Vitor Kato <vitor48kato@gmail.com>
2020-07-17 21:49:57 -0300

    Projeto finalizado

---
+++ Projeto/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListaTarefas = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Texto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Situacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddTarefa = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtSituacao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaTarefas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListaTarefas
+            // 
+            this.dgvListaTarefas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaTarefas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Texto,
+            this.Situacao});
+            this.dgvListaTarefas.Location = new System.Drawing.Point(101, 34);
+            this.dgvListaTarefas.Name = "dgvListaTarefas";
+            this.dgvListaTarefas.Size = new System.Drawing.Size(413, 235);
+            this.dgvListaTarefas.TabIndex = 0;
+            this.dgvListaTarefas.DoubleClick += new System.EventHandler(this.dgvListaTarefas_DoubleClick);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // Texto
+            // 
+            this.Texto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Texto.DataPropertyName = "Texto";
+            this.Texto.HeaderText = "Texto";
+            this.Texto.Name = "Texto";
+            // 
+            // Situacao
+            // 
+            this.Situacao.DataPropertyName = "Situacao";
+            this.Situacao.HeaderText = "Situação";
+            this.Situacao.Name = "Situacao";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista de Tarefas";
+            // 
+            // btnAddTarefa
+            // 
+            this.btnAddTarefa.Location = new System.Drawing.Point(491, 323);
+            this.btnAddTarefa.Name = "btnAddTarefa";
+            this.btnAddTarefa.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTarefa.TabIndex = 2;
+            this.btnAddTarefa.Text = "Adicionar";
+            this.btnAddTarefa.UseVisualStyleBackColor = true;
+            this.btnAddTarefa.Click += new System.EventHandler(this.btnAddTarefa_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(270, 421);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(153, 325);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(332, 20);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // txtSituacao
+            // 
+            this.txtSituacao.Location = new System.Drawing.Point(153, 351);
+            this.txtSituacao.Name = "txtSituacao";
+            this.txtSituacao.Size = new System.Drawing.Size(332, 20);
+            this.txtSituacao.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Descrição";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Situação";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ADICIONAR/ALTERAR TAREFA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(258, 395);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "EXCLUIR TAREFA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 558);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSituacao);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAddTarefa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvListaTarefas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaTarefas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListaTarefas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddTarefa;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtSituacao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Texto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Situacao;
+    }
+}
+
---
+++ Projeto/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto
+{
+    public partial class Form1 : Form
+    {
+        TAREFA model = new TAREFA();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Limpar();
+            this.Text = "TODO App";
+            preencherDataGridView();
+        }
+
+        void Limpar()
+        {
+            txtDescricao.Text = txtSituacao.Text = "";
+            btnExcluir.Enabled = false;
+            btnAddTarefa.Text = "Adicionar";
+            label3.Text = "Situação";
+            model.id = 0;
+        }
+
+        private void btnAddTarefa_Click(object sender, EventArgs e)
+        {
+            model.texto = txtDescricao.Text.Trim();
+            model.situacao = txtSituacao.Text.Trim();
+
+            using (DBEntities db = new DBEntities())
+            {
+                if (model.id == 0)
+                    db.TAREFAS.Add(model);
+                else
+                    db.Entry(model).State = EntityState.Modified;
+                
+                db.SaveChanges();
+            }
+
+            Limpar();
+            preencherDataGridView();
+            MessageBox.Show("Atualizado!");
+        }
+
+        void preencherDataGridView()
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                dgvListaTarefas.DataSource = db.TAREFAS.ToList<TAREFA>();
+            }
+        }
+
+        private void dgvListaTarefas_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvListaTarefas.CurrentRow.Index != -1)
+            {
+                model.id = Convert.ToInt32(dgvListaTarefas.CurrentRow.Cells["id"].Value);
+
+                using (DBEntities db = new DBEntities())
+                {
+                    model = db.TAREFAS.Where(x => x.id == model.id).FirstOrDefault();
+                    txtDescricao.Text = model.texto;
+                    txtSituacao.Text = model.situacao;
+                }
+
+                btnExcluir.Enabled = true;
+                btnAddTarefa.Text = "Alterar";
+                label3.Text = "Nova Descrição";
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Quer mesmo deletar essa tarefa?", "TODO App", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.TAREFAS.Attach(model);
+
+                    db.TAREFAS.Remove(model);
+                    db.SaveChanges();
+                    preencherDataGridView();
+                    Limpar();
+                    MessageBox.Show("Deletado!");
+                }
+            }
+        }
+    }
+}
